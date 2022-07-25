5cdaddbb7335ce4b4e3545ade2892e47a46d9c05
gavg712 <gavg712@gmail.com>
2022-07-24 23:23:22 -0500

    fix requirements.py

--- requirements.py
+++ requirements.py
@@ -1,13 +1,6 @@
 asgiref==3.5.2
 backports.zoneinfo==0.2.1
 Django==3.2
-django-calculation==1.0.0
-logger==1.4
-pytz==2022.1
-sqlparse==0.4.2
-asgiref==3.5.2
-backports.zoneinfo==0.2.1
-Django==3.2
 logger==1.4
 pytz==2022.1
 sqlparse==0.4.2
